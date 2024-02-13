9fa3db68112ab1a6c4e1cc6b5e5ce17b53836586
twekberg <tekberg@gmail.com>
2024-02-12 19:56:11 -0800

    ran stats.py through pylint. Not done yet. Made sure it works.

--- stats.py
+++ stats.py
@@ -26,6 +26,9 @@ activities = {
 
 
 def build_parser():
+    """
+    Command line parser.
+    """
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=__doc__.strip())
@@ -38,6 +41,9 @@ def build_parser():
 
 
 def main(args):
+    """
+    Main processing function.
+    """
     con = db_open(args.database_file)
     #             0  1        2          3
     sql = 'SELECT id,video_id,start_time,duration FROM clips'
@@ -46,43 +52,46 @@ def main(args):
     bad_data = []
     fatal_error_count = 0
     while True:
-        rs = cur.fetchone()
-        if not rs:
+        results = cur.fetchone()
+        if not results:
             break
-        if not check_start(rs[2]):
-            bad_data.append((rs[0], rs[1], rs[2]))
+        if not check_start(results[2]):
+            bad_data.append((results[0], results[1], results[2]))
         try:
-            check_seconds(rs[3])
+            check_seconds(results[3])
         except Exception as arg:
-            (id, video_id, start_time, duration) = rs
+            (id, video_id, start_time, duration) = results
             sql = 'SELECT filename FROM videos WHERE id=:video_id'
-            cur.execute(sql, dict(video_id=video_id))
-            rs = cur.fetchone()
-            print(arg,(id, video_id, start_time, duration), rs[0])
+            cur.execute(sql, {'video_id': video_id})
+            results = cur.fetchone()
+            print(arg,(id, video_id, start_time, duration), results[0])
             fatal_error_count += 1
     if fatal_error_count:
-        exit(1)
+        sys.exit(1)
     if bad_data:
         print('Bad data:')
         cur = con.cursor()
         sql = 'SELECT id,filename FROM videos WHERE id=:video_id'
         for (clip_id, video_id, start_time) in bad_data:
-            cur.execute(sql, dict(video_id=video_id))
-            rs = cur.fetchone()
-            print(clip_id, video_id, start_time, rs[1])
+            cur.execute(sql, {'video_id': video_id})
+            results = cur.fetchone()
+            print(clip_id, video_id, start_time, results[1])
 
     # Make sure duration_seconds and start_frame have values.
-    sql = "SELECT id, start_time, duration FROM clips WHERE duration_seconds IS NULL OR start_frame IS NULL"
+    sql = "SELECT id, start_time, duration FROM clips " \
+          "WHERE duration_seconds IS NULL OR start_frame IS NULL"
     cur.execute(sql)
-    for rs in cur.fetchall():
+    for results in cur.fetchall():
         try:
-            (id, start_time, duration) = rs
+            (id, start_time, duration) = results
             start_frame = to_start_frame(start_time)
             duration_seconds = to_seconds(duration)
-            sql = 'UPDATE clips SET start_frame=:start_frame, duration_seconds=:duration_seconds WHERE id=:id'
-            cur.execute(sql, dict(id=id, start_frame=start_frame, duration_seconds=duration_seconds))
-        except Exception as e:
-            print('Exception %s: clip id=%s' % (e, id))
+            sql = 'UPDATE clips SET start_frame=:start_frame, ' \
+                  'duration_seconds=:duration_seconds WHERE id=:id'
+            cur.execute(sql, {'id': id, 'start_frame': start_frame,
+                              'duration_seconds': duration_seconds})
+        except Exception as exception:
+            print(f'{exception=}: clip id={id}')
     con.commit()
 
     details = [['Act', 'Secs', 'Count', 'Min', 'Max', 'Avg',  'StdDev', 'Variance']]
@@ -91,124 +100,143 @@ def main(args):
   sum(duration_seconds) / count(duration_seconds) "Avg"
 FROM clips GROUP BY activity ORDER BY 1"""
     cur.execute(sql)
-    for rs in cur.fetchall():
-        if not rs[1]:
+    for results in cur.fetchall():
+        if not results[1]:
             continue
-        detail = [rs[0], format_time(rs[1])] + ['\t'.join(('%5.1f' % (r,)).lstrip() for r in rs[2:])]
+        detail = [results[0], format_time(results[1])] + \
+                 ['\t'.join(f'{r:5.1f}'.lstrip() for r in results[2:])]
 
         sql = """SELECT AVG((clips.duration_seconds - sub.a) * (clips.duration_seconds - sub.a))
                    FROM clips, 
                    (SELECT AVG(duration_seconds) AS a FROM clips) AS sub
                  WHERE activity = :activity"""
-        cur.execute(sql, dict(activity = rs[0]))
-        rs2 = cur.fetchone()
-        details.append(detail + ['%5.1f' % (math.sqrt(rs2[0]),), '%5.1f' % (rs2[0],)])
-
+        cur.execute(sql, {'activity': results[0]})
+        results2 = cur.fetchone()
+        details.append(detail + [f'{math.sqrt(results2[0]):5.1f}',
+                                 f'{results2[0]:5.1f}'])
     sql = """SELECT  activity, sum(duration_seconds) "Sum" ,count(duration_seconds) "Count",
   min(duration_seconds) "Min", max(duration_seconds) "Max",
   sum(duration_seconds) / count(duration_seconds) "Avg"
 FROM clips ORDER BY 1"""
     cur.execute(sql)
-    rs = cur.fetchone()
-    total = rs[1]
-    detail = ['Total', format_time(total)] + ['\t'.join(('%5.1f' % (r,)).lstrip() for r in rs[2:])]
+    results = cur.fetchone()
+    total = results[1]
+    detail = ['Total', format_time(total)] + \
+             ['\t'.join(f'{r:5.1f}'.lstrip() for r in results[2:])]
 
     sql = """SELECT AVG((clips.duration_seconds - sub.a) * (clips.duration_seconds - sub.a))
                FROM clips, 
                (SELECT AVG(duration_seconds) AS a FROM clips) AS sub"""
     cur.execute(sql)
-    rs2 = cur.fetchone()
-    details.append(detail + ['%5.1f' % (math.sqrt(rs2[0]),), '%5.1f' % (rs2[0],)])
+    results2 = cur.fetchone()
+    details.append(detail + [f'{math.sqrt(results2[0]):5.1f}',
+                             f'{results2[0]:5.1f}'])
 
     for row in details:
         if len(row[1]) < 8:
             row[1] += '\t'
-        sys.stdout.write('%-17s' % (activities[row[0]] if row[0] in activities else row[0]))
+        sys.stdout.write(f'{activities[row[0]]:<17s}' if row[0] in activities else
+                         f'{row[0]:<17s}')
+        sys.stdout.write('\t')
         sys.stdout.write('\t'.join(row[1:]))
         sys.stdout.write('\n')
 
 
 def to_start_frame(start_time):
     """
-    Convert start_time to start_frame.
+    Convert start_time string to start_frame.
     """
-    m = [1, 30, 30 * 60, 30 * 60 * 60]
-    i = 0
+    times = [1, 30, 30 * 60, 30 * 60 * 60]
+    time_index = 0
     start_frame = 0
     for part in reversed(start_time.split(':')):
-        start_frame += int(part) * m[i]
-        i += 1
+        start_frame += int(part) * times[time_index]
+        time_index += 1
     return start_frame
 
 
-def to_seconds(s):
-    #               s  s    f  f
-    m = re.match('^(\d?\d):(\d\\d)$', s)
-    if m:
+def to_seconds(string):
+    """
+    Look at a seconds string to pull off the components.
+    Returns the number seconds with fractional frames.
+    """
+    #                s  s    f  f
+    match = re.match(r'^(\d?\d):(\d\d)$', string)
+    if match:
         hour = 0
         min = 0
-        sec = m.group(1)
-        frame = m.group(2)
+        sec = match.group(1)
+        frame = match.group(2)
     else:
-        #               m  m    s s    f  f
-        m = re.match('^(\d?\d):(\d\d):(\d\\d)$', s)
-        if m:
+        #                m  m    s s    f  f
+        match = re.match(r'^(\d?\d):(\d\d):(\d\d)$', string)
+        if match:
             hour = 0
-            min = m.group(1)
-            sec = m.group(2)
-            frame = m.group(3)
+            min = match.group(1)
+            sec = match.group(2)
+            frame = match.group(3)
         else:
-            #               h  h    m  m    s s    f  f
-            m = re.match('^(\d?\d):(\d?\d):(\d\d):(\d\\d)$', s)
-            if m:
-                hour = m.group(1)
-                min = m.group(2)
-                sec = m.group(3)
-                frame = m.group(4)
+            #                     h  h    m  m    s s    f  f
+            match = re.match(r'^(\d?\d):(\d?\d):(\d\d):(\d\d)$', string)
+            if match:
+                hour = match.group(1)
+                min = match.group(2)
+                sec = match.group(3)
+                frame = match.group(4)
             else:
-                return "Didn't match proper syntax: %s" % (s,)
+                return f"Didn't match proper syntax: {string=}"
     if int(frame) >= 30:
-        return 'Frame too large: %s in %s' % (frame, s)
+        return 'Frame too large: %s in %s' % (frame, string)
     return (int(hour) * 60 + int(min)) * 60 + int(sec) + (int(frame) / 30.0)
 
 
-def check_seconds(s):
-    #               s  s    f  f
-    m = re.match('^(\d?\d):(\d\\d)$', s)
-    if m:
+def check_seconds(string):
+    """
+    Look at a seconds string to pull off the components.
+    This only does validation
+    """
+    #                s  s    f  f
+    match = re.match(r'^(\d?\d):(\d\d)$', string)
+    if match:
         hour = 0
         min = 0
-        sec = m.group(1)
-        frame = m.group(2)
+        sec = match.group(1)
+        frame = match.group(2)
     else:
-        #               m  m    s s    f  f
-        m = re.match('^(\d?\d):(\d\d):(\d\\d)$', s)
-        if m:
+        #                     m  m    s s    f  f
+        match = re.match(r'^(\d?\d):(\d\d):(\d\d)$', string)
+        if match:
             hour = 0
-            min = m.group(1)
-            sec = m.group(2)
-            frame = m.group(3)
+            min = match.group(1)
+            sec = match.group(2)
+            frame = match.group(3)
         else:
-            #               h  h    m m    s s    f  f
-            m = re.match('^(\d?\d):(\d\d):(\d\d):(\d\\d)$', s)
-            if m:
-                hour = m.group(1)
-                min = m.group(2)
-                sec = m.group(3)
-                frame = m.group(4)
+            #                     h  h    m m    s s    f  f
+            match = re.match(r'^(\d?\d):(\d\d):(\d\d):(\d\d)$', string)
+            if match:
+                hour = match.group(1)
+                min = match.group(2)
+                sec = match.group(3)
+                frame = match.group(4)
             else:
-                raise Exception("Didn't match: %s" % (s,))
+                raise Exception(f"Didn't match: {string=}")
     if int(frame) >= 30:
-        raise Exception('Frame too large: %s in %s' % (frame, s))
+        raise Exception('Frame too large: %s in %s' % (frame, string))
     return (int(hour) * 60 + int(min)) * 60 + int(sec) + (int(frame) / 30.0)
 
 
 def check_start(t):
-    #                   h    min    m  sec  s  fra  f
-    return re.match('^(([123]:)?[0-5])?\d:[0-5]\d:[0-2]\d$', t)
-    
+    """
+    Runs re.match on the start time string.
+    """
+    #                    h    min    m  sec  s  fra  f
+    return re.match(r'^(([123]:)?[0-5])?\d:[0-5]\d:[0-2]\d$', t)
+
 
 def format_time(t):
+    """
+    Convert time in ms to a formatted string.
+    """
     ms =int(100 * ( t - int(t) ))
     t = int(t)
     sec = t % 60
